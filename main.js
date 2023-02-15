e761a731bc8a1d3f3fd997b724a8f44fdfea4c6e
Shreyansh3957 <106986807+Shreyansh3957@users.noreply.github.com>
2023-02-15 20:54:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
-function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.hide();
-  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/y1c2slq4W/model.json', modelLoaded);
+prediction_1 = ""
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
 }
 
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JiCnMynGt/model.json', modelLoaded);
 
 function modelLoaded() {
-  console.log('Model Loaded!');
+    console.log('Model Loaded!');
 }
 
-function draw() {
-  image(video, 0, 0, 300, 300);
-  classifier.classify(video, gotResult);
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
 }
-var previous_result = '';
 
-function gotResult(error, results) {
-  if (error) {
-    console.error(error);
-  } else {
-    
-      console.log(results);
-    speechsynthesisUtterance(speak_data);
-      synth.speak(utterThis);
-
-      document.getElementById("result_object_name").innerHTML = results[0].label;
-      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+function gotResult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("status").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+        if (results[0].label == "Entry Accepted") {
+            document.getElementById("update_emoji").innerHTML = "&#x1F637;";
+            if (results[0].label == "Entry Denied") {
+                document.getElementById("update_emoji").innerHTML = "&#x26d4;";
+            }
+        }
     }
-  }
+}
